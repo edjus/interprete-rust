d0504016ee985de9d8db9a7b39efdc17604d374b
Edson Justo <justo.edson@gmail.com>
2022-11-13 22:37:32 -0300

    add lein project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject interprete "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot interprete.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
